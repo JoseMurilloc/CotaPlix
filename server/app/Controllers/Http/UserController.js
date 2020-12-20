e02cd75252abb2c03cc6fd5d995d77e91ab68806
José Murillo <jooseemurillo@gmail.com>
2020-12-20 12:06:46 -0300

    ✍️ Verify email using

--- server/app/Controllers/Http/UserController.js
+++ server/app/Controllers/Http/UserController.js
@@ -3,8 +3,18 @@
 const User = use('App/Models/User')
 
 class UserController {
-  async store({ request }) {
+  async store({ request, response }) {
     const data = request.all()
+
+
+    const existEmail = await User.query()
+      .where('email', data.email)
+      .fetch()
+
+    if(existEmail) {
+      return response.status(400)
+        .json({ error: 'Esse email já esta sendo usado, porfavor tente com outro' })
+    }
 
     const user = await User.create(data)
 
