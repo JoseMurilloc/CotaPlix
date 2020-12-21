61646fda0f3d8852f578bf3e8f368b301f54f08a
José Murillo <jooseemurillo@gmail.com>
2020-12-21 15:45:57 -0300

    Yup login page

--- web/src/pages/login.tsx
+++ web/src/pages/login.tsx
@@ -8,6 +8,9 @@ import Input from '../../src/components/input';
 import Button from '../../src/components/button';
 import { useToast } from '../../src/hooks/toast';
 
+import * as Yup from 'yup';
+
+
 // interface SiginFormData {
 //   email: string;
 //   password: string;
@@ -26,30 +29,57 @@ const Login: React.FC = () => {
   async function handleLogin(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
     const credentials = { email, password }
+
+
+    const scheme = Yup.object().shape({
+      email: Yup.string().required('E-mail obrigatório').email('Digite um email válido'),
+      password: Yup.string().required('Senha é obrigatória'),
+    });
+
+    const isValid = await scheme.isValid(credentials);
     
-    const toastStatus = await sigIn(credentials);
+    if (isValid) {
+      const toastStatus = await sigIn(credentials);
 
-    switch (toastStatus) {
-      case true: {
-        addToast({
-          type: 'sucess',
-          title: 'Autenticação concluida com sucesso',
-          description: 'Você está logado na aplicação!'
-        });
+      switch (toastStatus) {
+        case true: {
+          addToast({
+            type: 'sucess',
+            title: 'Autenticação concluida com sucesso',
+            description: 'Você está logado na aplicação!'
+          });
 
-        router.push('/app/dashboard')
-        break;
+          router.push('/app/dashboard')
+          break;
+        }
+        case false: 
+          addToast({
+            type: 'error',
+            title: 'Erro na autenticação',
+            description: 'Suas credenciais estão incorretas, tente novamente!'
+          });
+          break;
+        default:
+          return;
       }
-      case false:
-        addToast({
-          type: 'error',
-          title: 'Erro na autenticação',
-          description: 'Suas credenciais estão incorretas, tente novamente!'
+    } else {
+      addToast({
+        type: 'error',
+        title: 'Yop error',
+        description: 'Formulario com dados errados ou sem os mesmo!'
+      });
+
+      try {
+        await scheme.validate(credentials, {
+          abortEarly: false
         });
-        break;
-      default:
-        return;
-    }      
+      } catch(err) {
+        if (err instanceof Yup.ValidationError) {
+          console.log(err)
+          return;
+         }
+      }
+    }     
   }
 
   return (
