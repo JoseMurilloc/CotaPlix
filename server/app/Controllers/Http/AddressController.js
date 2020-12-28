d0cb9baec8f0a362289a57a55af4de1d897bc319
José Murillo <jooseemurillo@gmail.com>
2020-12-28 15:52:35 -0300

    ⚡chore: Address exists then return address

--- server/app/Controllers/Http/AddressController.js
+++ server/app/Controllers/Http/AddressController.js
@@ -5,7 +5,19 @@ const Address = use('App/Models/Address')
 class AddressController {
   async store({ request }) {
 
-    const data = request.only(['uf', 'state', 'city', 'neighborhood', 'number']);
+    const data = request.only(
+      ['state', 'city', 'neighborhood', 'number', 'street']
+    );
+
+    const addressExist = await Address
+      .findBy({
+        street: data.street,
+        neighborhood: data.neighborhood
+      })
+
+    if (addressExist) {
+      return addressExist
+    }
 
     const address = await Address.create(data)
 
