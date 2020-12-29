80564ba1968a46c34d14bb4cb7eeb2305213c28f
José Murillo <jooseemurillo@gmail.com>
2020-12-28 21:46:49 -0300

    ✨ Instance of error AppError

--- server/app/Exceptions/Handler.js
+++ server/app/Exceptions/Handler.js
@@ -4,11 +4,21 @@ const BaseExceptionHandler = use('BaseExceptionHandler')
 const Youch = use('youch')
 const Env = use('Env')
 
+const AppError = require('../Exceptions/AppError')
+
 class ExceptionHandler extends BaseExceptionHandler {
   async handle (error, { request, response }) {
     if (error.name === 'ValidationException') {
      return response.status(error.status).json(error.messages)
     }
+
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message
+      })
+    }
+
     if (Env.get('NODE_ENV') === 'development') {
       const youch = new Youch(error, request.request)
       const errorJSON = await youch.toJSON()
